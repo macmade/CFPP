1c649f2394b6c98fa41986ffa58ed197705cbadb
macmade <macmade@xs-labs.com>
2015-07-01 18:29:30 +0200

    Unit tests for CF::Array.

--- Unit-Tests/Test-CFPP-Array.cpp
+++ Unit-Tests/Test-CFPP-Array.cpp
@@ -37,3 +37,156 @@
 #include <GoogleMock/GoogleMock.h>
 
 using namespace testing;
+
+TEST( CFPP_Array, CTOR )
+{
+    CF::Array a;
+    
+    ASSERT_TRUE( a.IsValid() );
+    ASSERT_EQ( a.GetCount(), 0 );
+}
+
+TEST( CFPP_Array, CTOR_CFType )
+{}
+
+TEST( CFPP_Array, CTOR_CFArray )
+{}
+
+TEST( CFPP_Array, CCTOR )
+{
+    CF::Array a1;
+    
+    a1 << "hello";
+    a1 << "world";
+    
+    ASSERT_TRUE( a1.IsValid() );
+    ASSERT_EQ( a1.GetCount(), 2 );
+    
+    {
+        CF::Array a2( a1 );
+        
+        ASSERT_TRUE( a1.IsValid() );
+        ASSERT_TRUE( a2.IsValid() );
+        ASSERT_EQ( a1.GetCount(), 2 );
+        ASSERT_EQ( a2.GetCount(), 2 );
+    }
+}
+
+TEST( CFPP_Array, MCTOR )
+{
+    CF::Array a1;
+    
+    a1 << "hello";
+    a1 << "world";
+    
+    ASSERT_TRUE( a1.IsValid() );
+    ASSERT_EQ( a1.GetCount(), 2 );
+    
+    {
+        CF::Array a2( std::move( a1 ) );
+        
+        ASSERT_FALSE( a1.IsValid() );
+        ASSERT_TRUE(  a2.IsValid() );
+        ASSERT_EQ( a2.GetCount(), 2 );
+    }
+}
+
+TEST( CFPP_Array, OperatorAssignArray )
+{}
+
+TEST( CFPP_Array, OperatorAssignCFType )
+{}
+
+TEST( CFPP_Array, OperatorAssignCFArray )
+{}
+
+TEST( CFPP_Array, OperatorPlusEqualArray )
+{}
+
+TEST( CFPP_Array, OperatorPlusEqualCFType )
+{}
+
+TEST( CFPP_Array, OperatorPlusEqualCFArray )
+{}
+
+TEST( CFPP_Array, OperatorLeftShiftCFType )
+{}
+
+TEST( CFPP_Array, OperatorLeftShiftCChar )
+{}
+
+TEST( CFPP_Array, OperatorLeftShiftString )
+{}
+
+TEST( CFPP_Array, OperatorLeftShiftNumber )
+{}
+
+TEST( CFPP_Array, OperatorSubscript )
+{}
+
+TEST( CFPP_Array, GetTypeID )
+{
+    CF::Array a;
+    
+    ASSERT_EQ( a.GetTypeID(), CFArrayGetTypeID() );
+}
+
+TEST( CFPP_Array, GetCFObject )
+{
+    CF::Array a1;
+    CF::Array a2( static_cast< CFArrayRef >( NULL ) );
+    
+    ASSERT_TRUE( a1.GetCFObject() != NULL );
+    ASSERT_TRUE( a2.GetCFObject() == NULL );
+    ASSERT_EQ( CFGetTypeID( a1.GetCFObject() ), CFArrayGetTypeID() );
+}
+
+TEST( CFPP_Array, GetCount )
+{}
+
+TEST( CFPP_Array, ContainsValue )
+{}
+
+TEST( CFPP_Array, RemoveAllValues )
+{}
+
+TEST( CFPP_Array, GetValueAtIndex )
+{}
+
+TEST( CFPP_Array, SetValueAtIndex )
+{}
+
+TEST( CFPP_Array, InsertValueAtIndex )
+{}
+
+TEST( CFPP_Array, AppendValue )
+{}
+
+TEST( CFPP_Array, RemoveValueAtIndex )
+{}
+
+TEST( CFPP_Array, AppendArray )
+{}
+
+TEST( CFPP_Array, ExchangeValuesAtIndices )
+{}
+
+TEST( CFPP_Array, GetValues )
+{}
+
+TEST( CFPP_Array, Swap )
+{
+    CF::Array a1;
+    CF::Array a2;
+    
+    a1 << "hello";
+    a1 << "world";
+    
+    ASSERT_EQ( a1.GetCount(), 2 );
+    ASSERT_EQ( a2.GetCount(), 0 );
+    
+    swap( a1, a2 );
+    
+    ASSERT_EQ( a1.GetCount(), 0 );
+    ASSERT_EQ( a2.GetCount(), 2 );
+}
