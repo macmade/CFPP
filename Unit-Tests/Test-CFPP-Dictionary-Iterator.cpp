aa16478bdf54df5875ba1dbe3321781c1ebfb0ba
macmade <macmade@xs-labs.com>
2015-07-20 15:00:20 +0200

    Unit test for CF::Dictionary::Iterator.

--- Unit-Tests/Test-CFPP-Dictionary-Iterator.cpp
+++ Unit-Tests/Test-CFPP-Dictionary-Iterator.cpp
@@ -50,9 +50,6 @@ TEST( CFPP_Dictionary_Iterator, DictionaryBegin )
     
     i = d3.begin();
     
-    CFShow( i.GetKey() );
-    CFShow( i.GetValue() );
-    
     ASSERT_TRUE( d1.begin().GetKey()   == NULL );
     ASSERT_TRUE( d1.begin().GetValue() == NULL );
     ASSERT_TRUE( d2.begin().GetKey()   == NULL );
@@ -189,55 +186,649 @@ TEST( CFPP_Dictionary_Iterator, OperatorAssign )
 }
 
 TEST( CFPP_Dictionary_Iterator, OperatorPrefixIncrement )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        CF::Dictionary::Iterator i3;
+        CF::Dictionary::Iterator i4;
+        
+        i1 = d.begin();
+        i2 = ++i1;
+        i4 = ++i3;
+        
+        ASSERT_TRUE( i1.GetKey()   != NULL );
+        ASSERT_TRUE( i1.GetValue() != NULL );
+        ASSERT_TRUE( i2.GetKey()   != NULL );
+        ASSERT_TRUE( i2.GetValue() != NULL );
+        ASSERT_TRUE( i3.GetKey()   == NULL );
+        ASSERT_TRUE( i3.GetValue() == NULL );
+        ASSERT_TRUE( i4.GetKey()   == NULL );
+        ASSERT_TRUE( i4.GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i1.GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorPostfixIncrement )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        CF::Dictionary::Iterator i3;
+        CF::Dictionary::Iterator i4;
+        
+        i1 = d.begin();
+        i2 = i1++;
+        i4 = i3++;
+        
+        ASSERT_TRUE( i1.GetKey()   != NULL );
+        ASSERT_TRUE( i1.GetValue() != NULL );
+        ASSERT_TRUE( i2.GetKey()   != NULL );
+        ASSERT_TRUE( i2.GetValue() != NULL );
+        ASSERT_TRUE( i3.GetKey()   == NULL );
+        ASSERT_TRUE( i3.GetValue() == NULL );
+        ASSERT_TRUE( i4.GetKey()   == NULL );
+        ASSERT_TRUE( i4.GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i1.GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorPrefixDecrement )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        CF::Dictionary::Iterator i3;
+        CF::Dictionary::Iterator i4;
+        
+        i1 = d.end();
+        i2 = --i1;
+        i4 = --i3;
+        
+        ASSERT_TRUE( i1.GetKey()   != NULL );
+        ASSERT_TRUE( i1.GetValue() != NULL );
+        ASSERT_TRUE( i2.GetKey()   != NULL );
+        ASSERT_TRUE( i2.GetValue() != NULL );
+        ASSERT_TRUE( i3.GetKey()   == NULL );
+        ASSERT_TRUE( i3.GetValue() == NULL );
+        ASSERT_TRUE( i4.GetKey()   == NULL );
+        ASSERT_TRUE( i4.GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i1.GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i2.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorPostfixDecrement )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        CF::Dictionary::Iterator i3;
+        CF::Dictionary::Iterator i4;
+        
+        i1 = d.end();
+        i2 = i1--;
+        i4 = i3--;
+        
+        ASSERT_TRUE( i1.GetKey()   != NULL );
+        ASSERT_TRUE( i1.GetValue() != NULL );
+        ASSERT_TRUE( i2.GetKey()   == NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+        ASSERT_TRUE( i3.GetKey()   == NULL );
+        ASSERT_TRUE( i3.GetValue() == NULL );
+        ASSERT_TRUE( i4.GetKey()   == NULL );
+        ASSERT_TRUE( i4.GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i1.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorPlusEqual )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    d << CF::Pair( "x", "foo" );
+    d << CF::Pair( "y", "bar" );
+    
+    {
+        CF::Dictionary::Iterator i;
+        
+        i = d.begin();
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+        
+        i += 1;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+        
+        i += 0;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+        
+        i += 2;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorMinusEqual )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    d << CF::Pair( "x", "foo" );
+    d << CF::Pair( "y", "bar" );
+    
+    {
+        CF::Dictionary::Iterator i;
+        
+        i = d.end();
+        
+        ASSERT_TRUE( i.GetKey()   == NULL );
+        ASSERT_TRUE( i.GetValue() == NULL );
+        
+        i -= 1;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+        
+        i -= 0;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+        
+        i -= 2;
+        
+        ASSERT_TRUE( i.GetKey()   != NULL );
+        ASSERT_TRUE( i.GetValue() != NULL );
+        ASSERT_TRUE( CF::String( i.GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorPlus )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    d << CF::Pair( "x", "foo" );
+    d << CF::Pair( "y", "bar" );
+    
+    {
+        CF::Dictionary::Iterator i;
+        
+        i = d.begin();
+        
+        ASSERT_TRUE( i.GetKey()                                     != NULL );
+        ASSERT_TRUE( i.GetValue()                                   != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 1 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 1 ) ).GetValue() != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 0 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 0 ) ).GetValue() != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 2 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( i + static_cast< CFIndex >( 2 ) ).GetValue() != NULL );
+        
+        ASSERT_TRUE( CF::String( i.GetKey()                                     ).IsValid() );
+        ASSERT_TRUE( CF::String( i.GetValue()                                   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 1 ) ).GetKey()   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 1 ) ).GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 0 ) ).GetKey()   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 0 ) ).GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 2 ) ).GetKey()   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i + static_cast< CFIndex >( 2 ) ).GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorMinus )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    d << CF::Pair( "x", "foo" );
+    d << CF::Pair( "y", "bar" );
+    
+    {
+        CF::Dictionary::Iterator i;
+        
+        i = d.end();
+        
+        ASSERT_TRUE( i.GetKey()                                     == NULL );
+        ASSERT_TRUE( i.GetValue()                                   == NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 1 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 1 ) ).GetValue() != NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 0 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 0 ) ).GetValue() == NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 2 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( i - static_cast< CFIndex >( 2 ) ).GetValue() != NULL );
+        
+        ASSERT_TRUE( CF::String( ( i - static_cast< CFIndex >( 1 ) ).GetKey()   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i - static_cast< CFIndex >( 1 ) ).GetValue() ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i - static_cast< CFIndex >( 2 ) ).GetKey()   ).IsValid() );
+        ASSERT_TRUE( CF::String( ( i - static_cast< CFIndex >( 2 ) ).GetValue() ).IsValid() );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorEqual )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2;
+    
+    d1 << CF::Pair( "foo", "hello, world" );
+    d1 << CF::Pair( "bar", "hello, universe" );
+    d1 << CF::Pair( "x", "foo" );
+    d1 << CF::Pair( "y", "bar" );
+    
+    d2 = d1;
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        ASSERT_TRUE( i1 == i1 );
+        ASSERT_TRUE( i1 == i2 );
+        
+        i1 = d1.begin();
+        i2 = d1.begin();
+        
+        ASSERT_TRUE( i1 == i1 );
+        ASSERT_TRUE( i1 == i2 );
+        
+        i2++;
+        
+        ASSERT_FALSE( i1 == i2 );
+        
+        i2--;
+        
+        ASSERT_TRUE( i1 == i2 );
+        
+        i1 = d1.begin();
+        i2 = d2.begin();
+        
+        ASSERT_FALSE( i1 == i2 );
+        
+        i1 = d1.begin();
+        
+        d1 << CF::Pair( "foobar", "foobar" );
+        
+        i2 = d1.begin();
+        
+        ASSERT_FALSE( i1 == i2 );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorNotEqual )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2;
+    
+    d1 << CF::Pair( "foo", "hello, world" );
+    d1 << CF::Pair( "bar", "hello, universe" );
+    d1 << CF::Pair( "x", "foo" );
+    d1 << CF::Pair( "y", "bar" );
+    
+    d2 = d1;
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        ASSERT_FALSE( i1 != i1 );
+        ASSERT_FALSE( i1 != i2 );
+        
+        i1 = d1.begin();
+        i2 = d1.begin();
+        
+        ASSERT_FALSE( i1 != i1 );
+        ASSERT_FALSE( i1 != i2 );
+        
+        i2++;
+        
+        ASSERT_TRUE( i1 != i2 );
+        
+        i2--;
+        
+        ASSERT_FALSE( i1 != i2 );
+        
+        i1 = d1.begin();
+        i2 = d2.begin();
+        
+        ASSERT_TRUE( i1 != i2 );
+        
+        i1 = d1.begin();
+        
+        d1 << CF::Pair( "foobar", "foobar" );
+        
+        i2 = d1.begin();
+        
+        ASSERT_TRUE( i1 != i2 );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, OperatorDereference )
-{}
-
-TEST( CFPP_Dictionary_Iterator, OperatorMemberAccess )
-{}
-
-TEST( CFPP_Dictionary_Iterator, OperatorCastToCFType )
-{}
-
-TEST( CFPP_Dictionary_Iterator, GetTypeID )
-{}
-
-TEST( CFPP_Dictionary_Iterator, GetCFObject )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2( static_cast< CFArrayRef >( NULL ) );
+    
+    d1 << CF::Pair( "foo", "hello, world" );
+    d1 << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        ASSERT_TRUE( ( *( i1 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( *( i1 ) ).GetValue() == NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetValue() == NULL );
+        
+        i1 = d1.begin();
+        i2 = d2.begin();
+        
+        ASSERT_TRUE( ( *( i1 ) ).GetKey()   != NULL );
+        ASSERT_TRUE( ( *( i1 ) ).GetValue() != NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( ( *( i1 ) ).GetKey() ).IsValid() );
+        ASSERT_TRUE( CF::String( ( *( i1 ) ).GetValue() ).IsValid() );
+        
+        i1--;
+        i2--;
+        
+        ASSERT_TRUE( ( *( i1 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( *( i1 ) ).GetValue() == NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetKey()   == NULL );
+        ASSERT_TRUE( ( *( i2 ) ).GetValue() == NULL );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, GetKey )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2( static_cast< CFArrayRef >( NULL ) );
+    
+    d1 << CF::Pair( "foo", "hello, world" );
+    d1 << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        ASSERT_TRUE( i1.GetKey() == NULL );
+        ASSERT_TRUE( i2.GetKey() == NULL );
+        
+        i1 = d1.begin();
+        i2 = d2.begin();
+        
+        ASSERT_TRUE( i1.GetKey() != NULL );
+        ASSERT_TRUE( i2.GetKey() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetKey() ).IsValid() );
+        
+        i1--;
+        i2--;
+        
+        ASSERT_TRUE( i1.GetKey() == NULL );
+        ASSERT_TRUE( i2.GetKey() == NULL );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, GetValue )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2( static_cast< CFArrayRef >( NULL ) );
+    
+    d1 << CF::Pair( "foo", "hello, world" );
+    d1 << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        ASSERT_TRUE( i1.GetValue() == NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+        
+        i1 = d1.begin();
+        i2 = d2.begin();
+        
+        ASSERT_TRUE( i1.GetValue() != NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+        
+        ASSERT_TRUE( CF::String( i1.GetValue() ).IsValid() );
+        
+        i1--;
+        i2--;
+        
+        ASSERT_TRUE( i1.GetValue() == NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+    }
+}
 
 TEST( CFPP_Dictionary_Iterator, Swap )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    d << CF::Pair( "bar", "hello, universe" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2;
+        
+        i1 = d.begin();
+        i2 = d.end();
+        
+        ASSERT_TRUE( i1 == d.begin() );
+        ASSERT_TRUE( i2 == d.end() );
+        
+        swap( i1, i2 );
+        
+        ASSERT_TRUE( i1 == d.end() );
+        ASSERT_TRUE( i2 == d.begin() );
+    }
+}
+
+TEST( CFPP_Dictionary_Iterator, TestIterate )
+{
+    CF::Dictionary           d1;
+    CF::Dictionary           d2( static_cast< CFDictionaryRef >( NULL ) );
+    CF::Dictionary           d3;
+    CF::Dictionary::Iterator it;
+    CFIndex                  i;
+    
+    d3 << CF::Pair( "foo", "hello, world" );
+    d3 << CF::Pair( "bar", "hello, universe" );
+    d3 << CF::Pair( "x",   "foo" );
+    d3 << CF::Pair( "y",   "bar" );
+    
+    {
+        i = 0;
+        
+        for( it = d1.begin(); it != d1.end(); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( it = d2.begin(); it != d2.end(); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( it = d3.begin(); it != d3.end(); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 4 );
+    }
+}
+
+#ifdef CFPP_HAS_CPP11
+TEST( CFPP_Dictionary_Iterator, TestIterateSTD )
+{
+    CF::Dictionary           d1;
+    CF::Dictionary           d2( static_cast< CFDictionaryRef >( NULL ) );
+    CF::Dictionary           d3;
+    CF::Dictionary::Iterator it;
+    CFIndex                  i;
+    
+    d3 << CF::Pair( "foo", "hello, world" );
+    d3 << CF::Pair( "bar", "hello, universe" );
+    d3 << CF::Pair( "x",   "foo" );
+    d3 << CF::Pair( "y",   "bar" );
+    
+    {
+        i = 0;
+        
+        for( it = std::begin( d1 ); it != std::end( d1 ); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( it = std::begin( d2 ); it != std::end( d2 ); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( it = std::begin( d3 ); it != std::end( d3 ); ++it )
+        {
+            i++;
+        }
+        
+        ASSERT_EQ( i, 4 );
+    }
+}
+#endif
+
+#ifdef CFPP_HAS_CPP11
+TEST( CFPP_Dictionary_Iterator, TestIterateCPP11 )
+{
+    CF::Dictionary           d1;
+    CF::Dictionary           d2( static_cast< CFDictionaryRef >( NULL ) );
+    CF::Dictionary           d3;
+    CF::Dictionary::Iterator it;
+    CFIndex                  i;
+    
+    d3 << CF::Pair( "foo", "hello, world" );
+    d3 << CF::Pair( "bar", "hello, universe" );
+    d3 << CF::Pair( "x",   "foo" );
+    d3 << CF::Pair( "y",   "bar" );
+    
+    {
+        i = 0;
+        
+        for( CF::Pair p: d1 )
+        {
+            ( void )p;
+            
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( CF::Pair p: d2 )
+        {
+            ( void )p;
+            
+            i++;
+        }
+        
+        ASSERT_EQ( i, 0 );
+    }
+    
+    {
+        i = 0;
+        
+        for( CF::Pair p: d3 )
+        {
+            ASSERT_TRUE( CF::String( p.GetKey() ).IsValid() );
+            ASSERT_TRUE( CF::String( p.GetValue() ).IsValid() );
+            
+            i++;
+        }
+        
+        ASSERT_EQ( i, 4 );
+    }
+}
+#endif
