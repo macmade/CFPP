e33a7379ab362e3aeaccd1f62d908c933c9767e6
macmade <macmade@xs-labs.com>
2015-07-20 10:42:03 +0200

    Unit tests for CF::Dictionary.

--- Unit-Tests/Test-CFPP-Dictionary-Iterator.cpp
+++ Unit-Tests/Test-CFPP-Dictionary-Iterator.cpp
@@ -39,20 +39,120 @@
 using namespace testing;
 
 TEST( CFPP_Dictionary_Iterator, DictionaryBegin )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2( static_cast< CFDictionaryRef >( NULL ) );
+    CF::Dictionary d3;
+    
+    d3 << CF::Pair( "foo", "hello, world" );
+    
+    CF::Dictionary::Iterator i;
+    
+    i = d3.begin();
+    
+    CFShow( i.GetKey() );
+    CFShow( i.GetValue() );
+    
+    ASSERT_TRUE( d1.begin().GetKey()   == NULL );
+    ASSERT_TRUE( d1.begin().GetValue() == NULL );
+    ASSERT_TRUE( d2.begin().GetKey()   == NULL );
+    ASSERT_TRUE( d2.begin().GetValue() == NULL );
+    ASSERT_TRUE( d3.begin().GetKey()   != NULL );
+    ASSERT_TRUE( d3.begin().GetValue() != NULL );
+    
+    ASSERT_TRUE( CF::String( d3.begin().GetKey() )   == "foo" );
+    ASSERT_TRUE( CF::String( d3.begin().GetValue() ) == "hello, world" );
+    
+    ASSERT_TRUE( d1.begin() == d1.end() );
+    ASSERT_TRUE( d2.begin() == d2.end() );
+    ASSERT_TRUE( d3.begin() != d3.end() );
+}
 
 TEST( CFPP_Dictionary_Iterator, DictionaryEnd )
-{}
+{
+    CF::Dictionary d1;
+    CF::Dictionary d2( static_cast< CFDictionaryRef >( NULL ) );
+    CF::Dictionary d3;
+    
+    d3 << CF::Pair( "foo", "hello, world" );
+    
+    ASSERT_TRUE( d1.end().GetKey()   == NULL );
+    ASSERT_TRUE( d1.end().GetValue() == NULL );
+    ASSERT_TRUE( d2.end().GetKey()   == NULL );
+    ASSERT_TRUE( d2.end().GetValue() == NULL );
+    ASSERT_TRUE( d3.end().GetKey()   == NULL );
+    ASSERT_TRUE( d3.end().GetValue() == NULL );
+    
+    ASSERT_TRUE( CF::String( d3.begin().GetKey() )   == "foo" );
+    ASSERT_TRUE( CF::String( d3.begin().GetValue() ) == "hello, world" );
+    
+    ASSERT_TRUE( d1.begin() == d1.end() );
+    ASSERT_TRUE( d2.begin() == d2.end() );
+    ASSERT_TRUE( d3.begin() != d3.end() );
+}
 
 TEST( CFPP_Dictionary_Iterator, CTOR )
-{}
+{
+    CF::Dictionary::Iterator i;
+    
+    ASSERT_TRUE( i.GetKey()   == NULL );
+    ASSERT_TRUE( i.GetValue() == NULL );
+}
 
 TEST( CFPP_Dictionary_Iterator, CCTOR )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2( i1 );
+        CF::Dictionary::Iterator i3( d.begin() );
+        CF::Dictionary::Iterator i4( i3 );
+        
+        ASSERT_TRUE( i1.GetKey()   == NULL );
+        ASSERT_TRUE( i1.GetValue() == NULL );
+        ASSERT_TRUE( i2.GetKey()   == NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+        ASSERT_TRUE( i3.GetKey()   != NULL );
+        ASSERT_TRUE( i3.GetValue() != NULL );
+        ASSERT_TRUE( i4.GetKey()   != NULL );
+        ASSERT_TRUE( i4.GetValue() != NULL );
+        
+        ASSERT_TRUE( CF::String( i3.GetKey() )   == "foo" );
+        ASSERT_TRUE( CF::String( i3.GetValue() ) == "hello, world" );
+        ASSERT_TRUE( CF::String( i4.GetKey() )   == "foo" );
+        ASSERT_TRUE( CF::String( i4.GetValue() ) == "hello, world" );
+    }
+}
 
 #ifdef CFPP_HAS_CPP11
 TEST( CFPP_Dictionary_Iterator, MCTOR )
-{}
+{
+    CF::Dictionary d;
+    
+    d << CF::Pair( "foo", "hello, world" );
+    
+    {
+        CF::Dictionary::Iterator i1;
+        CF::Dictionary::Iterator i2( std::move( i1 ) );
+        CF::Dictionary::Iterator i3( d.begin() );
+        CF::Dictionary::Iterator i4( std::move( i3 ) );
+        
+        ASSERT_TRUE( i1.GetKey()   == NULL );
+        ASSERT_TRUE( i1.GetValue() == NULL );
+        ASSERT_TRUE( i2.GetKey()   == NULL );
+        ASSERT_TRUE( i2.GetValue() == NULL );
+        ASSERT_TRUE( i3.GetKey()   == NULL );
+        ASSERT_TRUE( i3.GetValue() == NULL );
+        ASSERT_TRUE( i4.GetKey()   != NULL );
+        ASSERT_TRUE( i4.GetValue() != NULL );
+        
+        ASSERT_TRUE( CF::String( i4.GetKey() )   == "foo" );
+        ASSERT_TRUE( CF::String( i4.GetValue() ) == "hello, world" );
+    }
+}
 #endif
 
 TEST( CFPP_Dictionary_Iterator, OperatorAssign )
